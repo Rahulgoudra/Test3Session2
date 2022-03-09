a25cb314852dd9b431dde36cc5129f021d79bc8b
Rahul G <97090660+Rahulgoudra@users.noreply.github.com>
2022-03-09 03:37:08 +0000

    p1 done

--- p1final.c
+++ p1final.c
@@ -8,9 +8,7 @@ void input(int *num1, int *den1, int *num2, int *den2)
 }
 void add(int num1,int den1, int num2, int den2, int *num3, int *den3)
 {
-  num1 = num1 * den2;
-  num2 = num2 * den1;
-  *num3 = num1 + num2;
+  *num3 =num1*den2+num2*den1;
   *den3=den1 * den2;
 }
 void output(int num1, int den1, int num2, int den2, int num3, int den3)
